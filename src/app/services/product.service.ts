8ba222d7653b49119d7bdaac3427b8ff9b2dd21e
Melih VARILCI <varilci.melih@outlook.com>
2021-03-26 20:09:41 +0300

    Exception Middleware Added

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,4 +22,8 @@ export class ProductService {
     let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath)
   }
+
+  add(product:Product):Observable<ListResponseModel<Product>>{
+    return this.httpClient.post<ListResponseModel<Product>>(this.apiUrl+"products/add",product);
+  }
 }
